4476d0be3035b7f8a9dd31be6fe9a078c898c2c2
Dharay99 <omoparija99@gmail.com>
2022-10-13 06:35:49 -0700

    Task 1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,18 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums of all parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list valist;
+
+	va_start(valist, n0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
+	return (sum);
+}
